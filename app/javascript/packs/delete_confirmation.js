6684381b2579fc7f8fdc6d4e0e23b83853064cd7
Abedalrhman Rasem <47899207+alonbe44@users.noreply.github.com>
2024-06-19 21:49:25 +0500

    Update delete_confirmation.js

--- app/javascript/packs/delete_confirmation.js
+++ app/javascript/packs/delete_confirmation.js
@@ -1,22 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   let deleteButtons = document.querySelectorAll("form button.btn-danger");
-//   let confirmDeleteBtn = document.getElementById("confirmDeleteBtn");
-//   let formToSubmit;
-//
-//   deleteButtons.forEach(button => {
-//     button.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       formToSubmit = button.closest("form");
-//       new bootstrap.Modal(document.getElementById('deleteConfirmationModal')).show();
-//     });
-//   });
-//
-//   confirmDeleteBtn.addEventListener("click", () => {
-//     formToSubmit.submit();
-//   });
-// });
-
-
 // app/javascript/packs/delete_confirmation.js
 
 document.addEventListener('DOMContentLoaded', () => {
